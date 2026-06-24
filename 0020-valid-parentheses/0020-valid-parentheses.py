d3ed8cc2e35393ed342d0a75b4c634e0ef0c07d5
Prashant Kumar <prashantkumar102369@gmail.com>
2026-06-24 08:44:18 +0530

    Time: 0 ms (100%), Space: 19.2 MB (66.52%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,18 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         paran = {')':'(', '}':'{', ']':'['}
-        open_paran = paran.values()
         paran_stack = []
 
         for i in s:
-            if i in open_paran:
-                paran_stack.append(i)
-
-            if i not in open_paran:
+            if i in paran:
                 if not paran_stack:
                     return False
                 if paran_stack.pop() != paran[i]:
                     return False
-            
+            else:
+                paran_stack.append(i)
 
-        return len(paran_stack) == 0
+        return not paran_stack
