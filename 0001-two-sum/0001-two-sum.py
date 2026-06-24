e18dba0d9369b2b24369ae26812ab52b0333f992
Prashant Kumar <prashantkumar102369@gmail.com>
2026-06-24 23:33:19 +0530

    Time: 3482 ms (5%), Space: 19.7 MB (94.98%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        left = 0
+        right = 1
+
+        while left != right:
+            if right == len(nums):
+                right = left + 2
+                left+= 1
+            if nums[left] + nums[right] == target: return left, right
+            right+=1
